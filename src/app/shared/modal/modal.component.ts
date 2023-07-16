2b54dc030e5f3a4a198cf00929e1b453c16c7e3a
InnoSufiyan <innosufiyan@gmail.com>
2023-07-17 02:46:59 +0500

    first commit

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent implements OnInit {
+  constructor(public modal: ModalService) {
+    console.log(this.modal.visible);
+  }
+  ngOnInit(): void {}
+}
